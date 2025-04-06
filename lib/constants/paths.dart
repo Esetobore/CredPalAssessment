4cc9b3e3b6353cb72ea6afe8ff8ff0c1aba459f4
Esetobore <esetobore1@gmail.com>
2025-04-06 13:36:46 +0100

    Update Pubspe, Add Assets and Path constants

---
+++ lib/constants/paths.dart
@@ -0,0 +1,18 @@
+class AppPaths {
+  //Images
+  static const String circleLogo1 = "assets/images/circle_logo1.png";
+  static const String circleLogo2 = "assets/images/circle_logo2.png";
+  static const String circleLogo3 = "assets/images/circle_logo3.png";
+  static const String circleLogo4 = "assets/images/circle_logo4.png";
+
+  static const String product1 = "assets/images/product_1.png";
+  static const String product2 = "assets/images/product_2.png";
+
+  static const String merchant1 = "assets/images/merchants/orile_restaurant.png";
+  static const String merchant2 = "assets/images/merchants/slot.png";
+  static const String merchant3 = "assets/images/merchants/oga_bassey.png";
+
+  //Icons
+  static const String exclamationIcon = "assets/icons/exclamation_icon.svg";
+  static const String scanIcon = "assets/icons/scan_icon.svg";
+}
